4646f7f16ba8c40cca2f368817c7ace37e985ac2
Lorenzo Moro <lorenzo.moro@top-ix.org>
2025-03-05 22:21:35 +0100

    Updated script push_net46.sh

--- scripts/bgp/opt/src/push_net46.sh
+++ scripts/bgp/opt/src/push_net46.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 bgpctl network flush
-cat ../../../../ps.openaccessitalia.org-main/storage/download/ipv4.txt|while read ip;do bgpctl network add $ip localpref 120 community NO_EXPORT community BLACKHOLE; done
-cat ../../../../ps.openaccessitalia.org-main/storage/download/ipv6.txt|while read ip;do bgpctl network add $ip localpref 120 community NO_EXPORT community BLACKHOLE; done
+cat ../../../../ps.openaccessitalia.org-main/storage/download/ipv4.txt|while read ip;do bgpctl network add $ip community NO_EXPORT community BLACKHOLE; done
+cat ../../../../ps.openaccessitalia.org-main/storage/download/ipv6.txt|while read ip;do bgpctl network add $ip community NO_EXPORT community BLACKHOLE; done
